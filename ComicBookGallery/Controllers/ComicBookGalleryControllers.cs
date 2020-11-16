52fc02900e1d691f4f03034a1477359ddc76da2b
Mark Leary <leary_mark@icloud.com>
2020-11-15 23:27:23 -0600

    Added Comic Book Detail View - Now using Views instead of hardcoded controllers

--- ComicBookGallery/Controllers/ComicBookGalleryControllers.cs
+++ ComicBookGallery/Controllers/ComicBookGalleryControllers.cs
@@ -10,12 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
-            {
-                return Redirect("/");
-            };
-
-            return Content("Hello from the comic books controller");
+            return View();
         }
     }
 }
