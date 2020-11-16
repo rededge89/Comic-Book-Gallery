6b3378fa5c2bbf5e81486ca3136b5f6e9905603c
Mark Leary <leary_mark@icloud.com>
2020-11-15 22:52:26 -0600

    Added Detail action method

---
+++ ComicBookGallery/Controllers/ComicBookGalleryControllers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
+    }
+}
